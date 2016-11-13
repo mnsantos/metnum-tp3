6ce30db44fd143ba936ce5d09261d183151dd715
Martin Santos <martin.n.santos@gmail.com>
2016-11-13 11:25:15 -0300

    bla

---
+++ src/main.py
@@ -0,0 +1,153 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import os
+import sys
+
+paramsDir="../stats/params.txt"
+toolsDir="../tools"
+playerStatsScript=toolsDir + "/obtenerStatsJugadores.sh"
+teamStatsScript=toolsDir + "/obtenerStatsEquipos.sh"
+teamStatsDir="../estadisticasEquipos/teamStats"
+playerStatsDir="../estadisticasJugadores/playerStats"
+abreviaturasDir="../stats/abreviaturas"
+equiposDir="../stats/equipos"
+winRateDir="../stats/winrate"
+
+class TeamStats:
+    def __init__(self):
+        self.year = 0
+        self.winRate = 0
+        self.name = ""
+        self.stats = []
+        self.number = 0
+        self.longName = ""
+
+    def __str__(self):
+     return "name: " + str(self.name) + ", longName: " + str(self.longName) + ", number: " + str(self.number) + ", year: " + str(self.year) + ", winRate: " + str(self.winRate) + ", stats: " + str(self.stats)
+
+class PlayerStats:
+    def __init__(self):
+        self.year = 0
+        self.team = ""
+        self.name = ""
+        self.position = ""
+        self.stats = []
+
+    def __str__(self):
+        return "name: " + str(self.name) + ", position: " + str(self.position) + ", team: " + str(self.team) + ", year: " + str(self.year) + ", stats: " + str(self.stats)
+
+def buildPlayerStatsFromParams():
+    fo = open(paramsDir, "r")
+    line = fo.readline()
+    fo.close()
+    startEnd = [int(x) for x in line.split(" ")]
+    years = range(startEnd[0], startEnd[1]+1)
+    command = "./" + playerStatsScript + " playerStats"
+    os.system(command)
+    i = -1
+    players = []
+    with open(playerStatsDir, 'rb') as csvfile:
+        playerStats = csv.reader(csvfile, delimiter=',')
+        for row in playerStats:
+            if (row[0]=="Rk"):
+                i = i+1
+            else:
+                player = PlayerStats()
+                player.year = years[i]
+                player.name = row[1]
+                player.position = row[2]
+                player.team = row[3]
+                player.stats = [ float(x) for x in row[4:] ]
+                players.append(player)
+                print player
+    return players
+
+def buildTeamStatsFromParams():
+    fo = open(paramsDir, "r")
+    line = fo.readline()
+    fo.close()
+    startEnd = [int(x) for x in line.split(" ")]
+    years = range(startEnd[0], startEnd[1]+1)
+    command = "./" + teamStatsScript + " teamStats"
+    os.system(command)
+    i = -1
+    teams = []
+    with open(teamStatsDir, 'rb') as csvfile:
+        teamStats = csv.reader(csvfile, delimiter=',')
+        for row in teamStats:
+            if (row[0]=="Rk"):
+                i = i+1
+            else:
+                team = TeamStats()
+                team.year = years[i]
+                team.name, team.number = findName(row[1], team.year)
+                team.longName = row[1]
+                team.stats = [ float(x) for x in row[2:] ]
+                teams.append(team)
+    for team in teams:
+        with open(winRateDir+"/leagues_NBA_"+str(team.year)+"_winrate.csv", 'rb') as csvfile:
+            winRateStats = csv.reader(csvfile, delimiter=',')
+            for row in winRateStats:
+                if (int(row[0]) == team.number):
+                    team.winRate = float(row[1])
+                    break
+    return teams
+
+def findName(longName, year):
+    teamNumber = 0
+    with open(equiposDir+"/equipos_"+str(year)+".txt", 'rb') as csvfile:
+        equipos = csv.reader(csvfile, delimiter=',')
+        for row in equipos:
+            if (row[1]==longName):
+                teamNumber = int(row[0])
+    with open(abreviaturasDir+"/abreviaturas_"+str(year)+".txt", 'rb') as csvfile:
+        abreviaturas = csv.reader(csvfile, delimiter=',')
+        for row in abreviaturas:
+            if (row[0]==str(teamNumber)):
+                return row[1], teamNumber
+
+def cmlGrado1(teamsStats):
+    stats = [ x.stats + [1] for x in teamsStats]
+    winRates = [ x.winRate for x in teamsStats]
+    A = np.vstack(stats)
+    #print A
+    coeficients = np.linalg.lstsq(A, winRates)[0]
+    print teamsStats[3]
+    print sum(teamsStats[3].stats * coeficients[0:-1]) + coeficients[-1]
+    #print coeficients
+
+    return coeficients
+
+
+
+# def cmlGrado1(teamsStats, factorsToUse):
+#     stats = []
+#     winRates = []
+#     for teamStat in teamsStats:
+#         teamsFactorsToUse = [ value for key,value in teamStat.stats if key in factorsToUse]
+#         winRates.append(teamStat.winRate)
+#         stats.append(teamsFactorsToUse)
+#     A = np.vstack(stats).T
+#     coeficients = np.linalg.lstsq(A, winRates)[0]
+#     return coeficients
+
+def calcMse():
+    print "hola"
+
+def test():
+    x = np.array([0, 1, 2, 3])
+    y = np.array([-1, 0.2, 0.9, 2.1])
+    A = np.vstack((x, np.ones(len(x)))).T
+    m, c = np.linalg.lstsq(A, y)[0]
+    print m,c
+    plt.plot(x, y, 'o', label='Original data', markersize=10)
+    plt.plot(x, m*x + c, 'r', label='Fitted line')
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    #print findName("PhoenixSuns", 2016)
+    # teams = buildTeamStatsFromParams()
+    # cmlGrado1(teams)
+    buildPlayerStatsFromParams()
